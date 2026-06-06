581f384ed92f12aa73859fadbf5efca9d917c58c
TuiㅤKoㅤCóㅤTên <112756562+soctrungkien@users.noreply.github.com>
2026-06-06 11:34:40 +0700

    Update MainScreen.kt

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/main/MainScreen.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/main/MainScreen.kt
@@ -326,7 +326,7 @@ private fun <E: TitledNavKey> TopBar(
                 if (parent == null) {
                     if (festivals.isEmpty()) {
                         Text(
-                            text = BuildKeys.LAUNCHER_IDENTIFIER,
+                            text = BuildKeys.LAUNCHER_IDENTIFIER + " (Unofficial)",
                             style = style,
                             softWrap = softWarp,
                             maxLines = maxLines
